fbd026f90df2fa600548f35fb6c07cb59d38934e
NIC619 <twedusuck@gmail.com>
2018-06-07 20:21:26 +0800

    Fix gas_used calculation in py-evm transaction receipt

--- eth_tester/backends/pyevm/serializers.py
+++ eth_tester/backends/pyevm/serializers.py
@@ -91,7 +91,7 @@ def serialize_transaction_receipt(block, receipts, transaction, transaction_inde
     if transaction_index == 0:
         origin_gas = 0
     else:
-        origin_gas = receipt.gas_used - receipts[transaction_index - 1].gas_used
+        origin_gas = receipts[transaction_index - 1].gas_used
 
     return {
         "transaction_hash": transaction.hash,
